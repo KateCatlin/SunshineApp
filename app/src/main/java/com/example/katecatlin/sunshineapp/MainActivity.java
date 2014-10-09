5d7f02273ff6794a2ab216c7de042b1ff638ae47
Kate Catlin <kate.catlin@detroitlabs.com>
2014-10-09 07:40:08 -0400

    Adding fragment that was never created.

--- app/src/main/java/com/example/katecatlin/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/katecatlin/sunshineapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.katecatlin.sunshineapp;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends Activity {
@@ -32,5 +36,18 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
     }
 }
