c61ebcd639dd8816fe2822f0e3aab368fd1309f0
Kate Catlin <kate.catlin@detroitlabs.com>
2014-10-15 10:40:59 -0400

    Copying lots of code from Udacity github on the ForecastFragment java because they did not explain that part at all. RAWR.

--- app/src/main/java/com/example/katecatlin/sunshineapp/Forecast.java
+++ app/src/main/java/com/example/katecatlin/sunshineapp/Forecast.java
@@ -4,35 +4,17 @@ package com.example.katecatlin.sunshineapp;
  * Created by katecatlin on 10/9/14.
  */
 public class Forecast {
-    private String day;
-    private String weather;
-    private int highTemp;
-    private int lowTemp;
+    private String todaysString;
 
-    public Forecast(String day, String weather, int highTemp, int lowTemp) {
-        this.day = day;
-        this.weather = weather;
-        this.highTemp = highTemp;
-        this.lowTemp = lowTemp;
+
+    public Forecast(String forecast) {
+        this.todaysString = forecast;
     }
 
     public String getDay() {
-        return day;
+        return todaysString;
     }
 
-    public String getWeather() {
-        return weather;
-    }
+    public String getTodaysString() { return todaysString;}
 
-    public int getHighTemp() {
-        return highTemp;
-    }
-
-    public int getLowTemp() {
-        return lowTemp;
-    }
-
-    public String toString() {
-        return getDay()+ ": " + getWeather() + " " + getHighTemp() + "/" + getLowTemp();
-    }
 }
